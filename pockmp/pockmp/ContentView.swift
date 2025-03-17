aab3376441df9d44a1337dbcab3c1faed6f91fbe
Luis Eduardo <luis.fernandes@aiqfome.com>
2025-03-17 16:16:39 -0300

    first commit ios app

---
+++ pockmp/pockmp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pockmp
+//
+//  Created by Luís Eduardo Marinho Fernandes on 17/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pockmp/pockmp/pockmpApp.swift
@@ -0,0 +1,17 @@
+//
+//  pockmpApp.swift
+//  pockmp
+//
+//  Created by Luís Eduardo Marinho Fernandes on 17/03/25.
+//
+
+import SwiftUI
+
+@main
+struct pockmpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
